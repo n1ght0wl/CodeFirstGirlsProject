a6f38d9b69df2c952116bf5e16295b11ea11d0bd
Sandy Di Yu <sandy.d.yu@gmail.com>
2019-04-08 19:23:16 +0100

    added popup menu to each page
    affirmations still doesn't work

--- js/drops.js
+++ js/drops.js
@@ -39,3 +39,8 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-137706981-1');
